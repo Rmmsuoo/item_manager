118d91ac6b00c8b58f9d23398f13865dc211ca0f
Noboru Shimomura <rmmsuoorbnauoi@gmail.com>
2023-09-11 05:23:34 +0900

    lesson03実装完了

--- src/main/java/com/example/service/ItemService.java
+++ src/main/java/com/example/service/ItemService.java
@@ -22,4 +22,24 @@ public class ItemService {
 	public List<Item> findAll() {
 	    return this.itemRepository.findAll();
 	}
+	public Item save(ItemForm itemForm) {
+		Item item = new Item();
+		item.setName(itemForm.getName());
+		item.setPrice(itemForm.getPrice());
+		return this.itemRepository.save(item);
+	}
+	public Item findById(Integer id) {
+		Optional<Item> optionalItem = this.itemRepository.findById(id);
+		Item item = optionalItem.get();
+		return item;
+	}
+	public Item update(Integer id,ItemForm itemForm) {
+		Item item = this.findById(id);
+		item.setName(itemForm.getName());
+		item.setPrice(itemForm.getPrice());
+		return this.itemRepository.save(item);
+	}
+	public void delete(Integer id) {
+		this.itemRepository.deleteById(id);
+	}
 }
