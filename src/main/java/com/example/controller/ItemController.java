4ff9a033c5063bc3e4ba2e86fb06bd370b16ef61
Noboru Shimomura <rmmsuoorbnauoi@gmail.com>
2023-09-12 15:56:42 +0900

    lesson06実装完了

--- src/main/java/com/example/controller/ItemController.java
+++ src/main/java/com/example/controller/ItemController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.entity.Category;
 import com.example.entity.Item;
@@ -20,41 +21,44 @@ import com.example.service.ItemService;
 @Controller
 @RequestMapping("/item")
 public class ItemController {
-	
+
 	private final ItemService itemService;
 	private final CategoryService categoryService;
 
-    @Autowired
-    public ItemController(ItemService itemService, CategoryService categoryService) {
-        this.itemService = itemService;
-        this.categoryService = categoryService;
-    }
-    
-//	商品一覧表示
+	@Autowired
+	public ItemController(ItemService itemService, CategoryService categoryService) {
+		this.itemService = itemService;
+		this.categoryService = categoryService;
+	}
+
+	//	商品一覧表示
 	@GetMapping
 	public String index(Model model) {
 		// データの疎通確認
-	    List<Item> items = this.itemService.findByDeletedAtIsNull();
-	    // コンソールよりListの中身を確認する
-	    model.addAttribute("items", items);
-//	    System.out.println(items.toString());
+		List<Item> items = this.itemService.findByDeletedAtIsNull();
+		// コンソールよりListの中身を確認する
+		model.addAttribute("items", items);
+		//	    System.out.println(items.toString());
 		return "item/index";
 	}
-//	商品登録ページ
+
+	//	商品登録ページ
 	@GetMapping("toroku")
-	public String torokuPage(@ModelAttribute("itemForm") ItemForm itemForm,Model model) {
+	public String torokuPage(@ModelAttribute("itemForm") ItemForm itemForm, Model model) {
 		List<Category> categories = this.categoryService.findAll();
-		
-		model.addAttribute("categories",categories);
+
+		model.addAttribute("categories", categories);
 		return "item/torokuPage";
 	}
-//	商品登録の実行
+
+	//	商品登録の実行
 	@PostMapping("toroku")
 	public String toroku(ItemForm itemForm) {
 		this.itemService.save(itemForm);
 		return "redirect:/item";
 	}
-//	商品編集ページ
+
+	//	商品編集ページ
 	@GetMapping("henshu/{id}")
 	public String henshu(@PathVariable("id") Integer id, Model model, @ModelAttribute("itemForm") ItemForm itemForm) {
 		Item item = this.itemService.findById(id);
@@ -62,20 +66,36 @@ public class ItemController {
 		itemForm.setPrice(item.getPrice());
 		itemForm.setCategoryId(item.getCategoryId());
 		List<Category> categories = this.categoryService.findAll();
-		model.addAttribute("id",id);
-		model.addAttribute("categories",categories);
+		model.addAttribute("id", id);
+		model.addAttribute("categories", categories);
 		return "item/henshuPage";
 	}
-//	商品編集の実行
-	@PostMapping("henshu/{id}")  
-	public String henshu(@PathVariable("id") Integer id,@ModelAttribute("itemForm") ItemForm itemForm) {
+
+	//	商品編集の実行
+	@PostMapping("henshu/{id}")
+	public String henshu(@PathVariable("id") Integer id, @ModelAttribute("itemForm") ItemForm itemForm) {
 		this.itemService.update(id, itemForm);
 		return "redirect:/item";
 	}
-//	商品削除の実行
-	@PostMapping("sakujo/{id}")  
+
+	//	商品削除の実行
+	@PostMapping("sakujo/{id}")
 	public String sakujo(@PathVariable("id") Integer id) {
 		this.itemService.delete(id);
+		return "redirect:/item";
+	}
+
+	//入荷処理の実行
+	@PostMapping(path = "stock/{id}", params = "in")
+	public String nyuka(@PathVariable("id") Integer id, @RequestParam("stock") Integer inputValue) {
+		this.itemService.nyuka(id, inputValue);
+		return "redirect:/item";
+	}
+
+	//出荷処理の実行
+	@PostMapping(path = "stock/{id}", params = "out")
+	public String shukka(@PathVariable("id") Integer id, @RequestParam("stock") Integer inputValue) {
+		this.itemService.shukka(id, inputValue);
 		return "redirect:/item";
 	}
 }
